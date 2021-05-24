907d0b8b2350875f4cb26ad9925185b7870b843e
Rakesh Maurya <rax215@gmail.com>
2021-05-24 16:58:13 +0530

    Dropdown for primeReact

--- components/DropDown/DropDown.js
+++ components/DropDown/DropDown.js
@@ -1,21 +1,32 @@
 
 const getDropDown = (attributes) => {
+    let component = ''
+    if(attributes.library === 'materialUi') {
+        component = ` <Grid item lg={4} className="wrapper">
+        <InputLabel id="${attributes.id}Label" required={${attributes.mandatory}}> ${attributes.label} </InputLabel>
+        <Select id="${attributes.id}" value={values.${attributes.id}}>
+            <MenuItem value="" disabled>
+                <em>- Select -</em>
+            </MenuItem>    
+            {componentOptions.${attributes.id}Options.map(item =>
+                <MenuItem key={item} value={item}>{item}</MenuItem>        
+            )}
+        </Select>
+   </Grid>
+`
+    } else if(attributes.library === 'primeReact') {
+        component = `<label htmlFor="${attributes.id}Label">${attributes.label}</label>
+        <Dropdown value={values.${attributes.id}} options={componentOptions.${attributes.id}Options.map(item => {let resp = {}
+            resp.label = item
+            resp.value = item              
+            return resp
+          })} placeholder="Select an option" />`
+    }
 
-    let lebels = attributes.options
-    let lebelName = lebels.split(',')
     return (
-        ` <Grid item lg={4} className="wrapper">
-                <InputLabel id="${attributes.id}Label" required={${attributes.mandatory}}> ${attributes.label} </InputLabel>
-                <Select id="${attributes.id}" value={values.${attributes.id}}>
-                    <MenuItem value="" disabled>
-                        <em>- Select -</em>
-                    </MenuItem>    
-                    {componentOptions.${attributes.id}Options.map(item =>
-                        <MenuItem key={item} value={item}>{item}</MenuItem>        
-                    )}
-                </Select>
-           </Grid>
-    `
+        `<div>
+            ${component}
+        </div>`
     )
 }
 
