531eeec3d8a36c96e1841e8a0e7f3353c890b488
Rajnish kumar <rajnishkumarblore@gmail.com>
2021-05-24 23:39:14 +0530

    input text field updated for prime reat library

--- components/TextInput/TextInput.js
+++ components/TextInput/TextInput.js
@@ -1,14 +1,24 @@
 const FormLabel = require("../FormLabel/FormLabel");
 
 const getTextField = (attributes) => {
-  return `<Grid item xs={2} alignItems="flex-start" className="wrapper">   
+  let component = ''
+  if (attributes.library === 'materialUi') {
+    component = `<Grid item xs={2} alignItems="flex-start" className="wrapper">   
   ${FormLabel.getFormLebel(attributes)} 
   </Grid>
   <Grid item xs={4} alignItems="flex-start" className="wrapper">       
-        <TextField id="${
-          attributes.id
-        }" className="formlabel" variant="outlined" style={{ minWidth: 278 }}/>
+        <TextField id="${attributes.id
+      }" className="formlabel" variant="outlined" style={{ minWidth: 278 }}/>
     </Grid>`;
-};
+  } else if (attributes.library === 'primeReact') {
+    component = `<label htmlFor="${attributes.id}Label">${attributes.label}</label>
+    <TextField id="${attributes.id
+      }" className="formlabel" variant="outlined" style={{ minWidth: 278 }}/>`
 
+  };
+
+  return (`<div>
+  ${component}
+  </div>`)
+}
 module.exports = { getTextField };
