a8929a0c23b7d2cb9b7c6fa5ee6097f8f77ae17e
Rohan Tarai <rohantarai@users.noreply.github.com>
2021-09-16 04:46:01 +0530

    added functionality to upload template file

--- generator.js
+++ generator.js
@@ -1,21 +1,21 @@
-const { generateJSX } = require("./generateJSX");
-const fs = require("fs");
-const path = require("path");
-const CommonUtil = require("./commonUtil");
-const parser = new (require("simple-excel-to-json").XlsParser)();
+const { generateJSX } = require('./generateJSX');
+const fs = require('fs');
+const path = require('path');
+const CommonUtil = require('./commonUtil');
+const parser = new (require('simple-excel-to-json').XlsParser)();
 
-let projName = "";
+let projName = '';
 
 let indexImport = `import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
-import reportWebVitals from './reportWebVitals';`
+import reportWebVitals from './reportWebVitals';`;
 
 const primeReactImport = `import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';`
+import 'primeicons/primeicons.css';`;
 
 const indexImportAdd = `ReactDOM.render(
   <React.StrictMode>
@@ -24,32 +24,32 @@ const indexImportAdd = `ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();`
+reportWebVitals();`;
 
 const materialUIDependencies = `"@material-ui/core": "^4.11.3",
 "@material-ui/icons": "^4.11.2",
-"@material-ui/pickers": "^3.3.10",`
+"@material-ui/pickers": "^3.3.10",`;
 
 const primeReactDependencies = `"primeflex": "^2.0.0",
 "primereact": "^6.3.2",
 "primeicons": "^4.1.0",
-"react-transition-group": "^4.4.2",`
+"react-transition-group": "^4.4.2",`;
 
-getDependencies = (masterLayout) => {
-  if(masterLayout.library === 'primeReact') {
-    return primeReactDependencies
-  } else  if(masterLayout.library === 'materialUi') {
-    return materialUIDependencies
+const getDependencies = (masterLayout) => {
+  if (masterLayout.library === 'primeReact') {
+    return primeReactDependencies;
+  } else if (masterLayout.library === 'materialUI') {
+    return materialUIDependencies;
   }
-}
+};
 
-const getIndexData = masterLayout => {
-  if(masterLayout.library === 'primeReact') {
-    return indexImport + '\n' + primeReactImport +'\n' + indexImportAdd
+const getIndexData = (masterLayout) => {
+  if (masterLayout.library === 'primeReact') {
+    return indexImport + '\n' + primeReactImport + '\n' + indexImportAdd;
   } else {
-    return indexImport + '\n' + indexImportAdd
+    return indexImport + '\n' + indexImportAdd;
   }
-}
+};
 
 const generateJSXFile = async (masterLayout) => {
   const result = await generateJSX(masterLayout);
@@ -58,7 +58,7 @@ const generateJSXFile = async (masterLayout) => {
 
 const readExcelFile = () => {
   // .xlsx file path
-  const filePath = path.join(__dirname, "react_template.xlsx");
+  const filePath = path.join(`${__dirname}/uploads`, 'react_template.xlsx');
 
   // parsing excel file
   var data = parser.parseXls2Json(filePath, {
@@ -73,51 +73,39 @@ const readExcelFile = () => {
 
 const reactGenerator = async () => {
   return new Promise((resolve, reject) => {
-    const directory = "./output/";
+    const directory = './output/';
     fs.mkdir(directory, (err) => {
       if (err) {
-        console.log("error while creating project directory -1");
+        console.log('error while creating project directory -1');
       } else {
         const masterLayout = readExcelFile();
 
         projName = masterLayout.componentName;
-        layout = masterLayout.layout || 'row'
+        let layout = masterLayout.layout || 'row';
 
         fs.mkdir(`./output/${projName}`, (err) => {
           if (err) {
-            console.log("error while creating project directory -2");
+            console.log('error while creating project directory -2');
           } else {
-            console.log("project directory created");
+            console.log('project directory created');
             const dirName = __dirname;
             fs.mkdir(`./output/${projName}/src`, (err) => {
               if (err) {
-                console.log("error while creating src directory -3");
+                console.log('error while creating src directory -3');
               } else {
-                console.log("src directory created");
+                console.log('src directory created');
 
-                fs.copyFile(
-                  `${dirName}/template/App.css`,
-                  `./output/${projName}/src/App.css`,
-                  (err) => {
-                    if (err) throw err;
-                  }
-                );
+                fs.copyFile(`${dirName}/template/App.css`, `./output/${projName}/src/App.css`, (err) => {
+                  if (err) throw err;
+                });
 
-                fs.copyFile(
-                  `${dirName}/template/${layout}.css`,
-                  `./output/${projName}/src/${layout}.css`,
-                  (err) => {
-                    if (err) throw err;
-                  }
-                );
+                fs.copyFile(`${dirName}/template/${layout}.css`, `./output/${projName}/src/${layout}.css`, (err) => {
+                  if (err) throw err;
+                });
 
-                fs.copyFile(
-                  `${dirName}/template/App.js`,
-                  `./output/${projName}/src/App.js`,
-                  (err) => {
-                    if (err) throw err;
-                  }
-                );
+                fs.copyFile(`${dirName}/template/App.js`, `./output/${projName}/src/App.js`, (err) => {
+                  if (err) throw err;
+                });
 
                 // fs.copyFile(`${dirName}/template/index.js`, `./output/${projName}/src/index.js`, (err) => {
                 //   if (err) throw err;
@@ -137,75 +125,48 @@ const reactGenerator = async () => {
 
                 fs.mkdir(`./output/${projName}/public`, (err) => {
                   if (err) {
-                    console.log("error while creating src directory -4");
+                    console.log('error while creating src directory -4');
                   } else {
-                    console.log("public directory created");
-                    fs.copyFile(
-                      `${dirName}/template/index.html`,
-                      `./output/${projName}/public/index.html`,
-                      (err) => {
-                        if (err) throw err;
+                    console.log('public directory created');
+                    fs.copyFile(`${dirName}/template/index.html`, `./output/${projName}/public/index.html`, (err) => {
+                      if (err) throw err;
 
-                        generateJSXFile(masterLayout).then((data) => {
-                          fs.writeFile(
-                            path.join(
-                              `./output/${projName}/src/`,
-                              `${projName}.js`
-                            ),
-                            data,
-                            (err) => {
-                              // throws an error, you could also catch it here
-                              if (err) throw err;
+                      generateJSXFile(masterLayout).then((data) => {
+                        fs.writeFile(path.join(`./output/${projName}/src/`, `${projName}.js`), data, (err) => {
+                          // throws an error, you could also catch it here
+                          if (err) throw err;
 
-                              // the file was saved
-                              console.log("jsx file contents written");
-                              resolve();
-                            }
-                          );
+                          // the file was saved
+                          console.log('jsx file contents written');
+                          resolve();
                         });
-                      }
-                    );
+                      });
+                    });
                   }
                 });
 
                 //generate package.json
-                fs.readFile(
-                  `${dirName}/template/package.json`,
-                  "utf8",
-                  (err, data) => {
+                fs.readFile(`${dirName}/template/package.json`, 'utf8', (err, data) => {
+                  if (err) throw err;
+                  let pkg = data.replace('<app-name>', projName);
+                  let newPkg = pkg.replace('<app-dependencies>', getDependencies(masterLayout));
+                  fs.writeFile(`./output/${projName}/package.json`, newPkg, (err) => {
                     if (err) throw err;
-                    let pkg = data.replace("<app-name>", projName);
-                    let newPkg = pkg.replace("<app-dependencies>", getDependencies(masterLayout))
-                    fs.writeFile(
-                      `./output/${projName}/package.json`,
-                      newPkg,
-                      (err) => {
-                        if (err) throw err;
-                      }
-                    );
-                  }
-                );
+                  });
+                });
 
                 // fs.copyFile(`${dirName}/template/package.json`, `./output/${projName}/package.json`, (err) => {
                 //   if (err) throw err;
                 // });
 
                 //generate Home.js
-                fs.readFile(
-                  `${dirName}/template/Home.js`,
-                  "utf8",
-                  (err, data) => {
+                fs.readFile(`${dirName}/template/Home.js`, 'utf8', (err, data) => {
+                  if (err) throw err;
+                  let pkg = data.replace(/<app-name>/g, projName);
+                  fs.writeFile(`./output/${projName}/src/Home.js`, pkg, (err) => {
                     if (err) throw err;
-                    let pkg = data.replace(/<app-name>/g, projName);
-                    fs.writeFile(
-                      `./output/${projName}/src/Home.js`,
-                      pkg,
-                      (err) => {
-                        if (err) throw err;
-                      }
-                    );
-                  }
-                );
+                  });
+                });
 
                 // fs.copyFile(`${dirName}/template/package.json`, `./output/${projName}/package.json`, (err) => {
                 //   if (err) throw err;
